2319fc84f2e200dcf70cfa72bed3f66809613d49
svitalis123 <vitalismutwiri@gmail.com>
2022-05-30 04:41:47 -0700

    Run Python file 1. Run inline 2. Hello, print 3. Print integer 4. Print float 5. Print string 6. Play with strings 7. Copy - Cut - Paste 8. Create a new sentence 9. Easter Egg 10. Linked list cycle 11. Hello, write 12. Compile 13. ByteCode -> Python #1

---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+number = 3.14159
+decimalfloat="{:.2f}".format(number)
+print(f"Float: {decimalfloat}")
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+str2=str*3
+print(f"{str2}")
+print(str[0:10])
