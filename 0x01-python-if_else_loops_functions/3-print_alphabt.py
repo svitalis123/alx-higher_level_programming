3817a1d74a9197a7924663b3565920d8257f9b05
svitalis123 <vitalismutwiri@gmail.com>
2022-05-31 02:46:10 -0700

    a

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alpha_letters in range(97, 123):
-      if alpha_letters == ord('e') or alpha_letters == ord('q'):
+    if alpha_letters == ord('e') or alpha_letters == ord('q'):
          continue
       print("{:c}".format(alpha_letters), end="")
