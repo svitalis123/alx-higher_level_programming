183092e3192759513a3265ef0a3c9c079a5e5942
svitalis123 <vitalismutwiri@gmail.com>
2022-06-08 04:39:21 -0700

    update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """
+    Updates a dictionary with a new key and value
+    """
+    a_dictionary[key] = value
+    return a_dictionary
