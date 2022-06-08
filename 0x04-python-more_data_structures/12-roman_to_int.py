7ed96da0637ea5a1a8ad1d45adca39babe2050c5
svitalis123 <vitalismutwiri@gmail.com>
2022-06-08 05:02:48 -0700

    12. Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """
+    Convert a roman number to an integer
+    """
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_int = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
+            roman_int += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
+        else:
+            roman_int += roman_dict[roman_string[i]]
+    return roman_int
