4a0bcabdecdfb4824124b2b0f37219ca1050caae
svitalis123 <vitalismutwiri@gmail.com>
2022-06-08 05:18:50 -0700

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     """
-    Convert a roman number to an integer
+    Roman to Integer
     """
-    roman_dict = {'I': 1, 'V': 5, 'X': 10,
-            'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_int = 0
-    for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            roman_int += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
-        else:
-            roman_int += roman_dict[roman_string[i]]
-    return roman_int
+    r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_string = roman_string.upper()
+    last_value = 0
+    result = 0
+    for char in roman_string:
+        value = r[char]
+        result += value
+        if value > last_value:
+            result -= 2 * last_value
+            last_value = value
+    return result
